8078d137102079fa2ad570b54e087f72e459714e
Danial Al Ghazali Walangadi <danialagw@students.unnes.ac.id>
2024-02-25 17:59:49 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    std::vector<char> alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
+                                  'v','w','x','y','z'};
+
+    std::string result;
+    std::string code;
+    std::getline(std::cin, code);
+    for(char c : code){
+        for(int i = 0 ; i < (int)alphabet.size() ; i++){
+            if(c == alphabet[i] && (i+1)%2==0){
+                result += alphabet[i-1];
+            } else if(c == alphabet[i] && (i+1)%2!=0){
+                result += alphabet[i+1];
+            } else if(c == ' '){break;}
+            else{continue;}
+        }
+        if(c == ' '){result += " ";}
+    }
+    std::cout<<result;
+}
